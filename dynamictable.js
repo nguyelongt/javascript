7c466802093bb8b99131d352ff4173e2b59b5ae6
NguyeLongT <nguyelongt@gmail.com>
2017-08-09 15:27:54 -0400

    Update

---
+++ dynamictable.js
@@ -0,0 +1,38 @@
+function elementID(id) {
+  return document.getElementById(id);
+}
+
+var friends = {}
+var array = []
+
+function pushNameArray() {
+
+  array = [];
+
+  var nameStored = elementID("nameInput");
+  var emailStored = elementID("emailInput");
+  var cellStored = elementID("cellInput");
+      friends.Name = nameStored.value;
+      friends.Email = emailStored.value;
+      friends.Cell = cellStored.value;
+      array.push(friends);
+}
+
+function displayOnDocument() {
+  for (var i = 0; i < array.length; i++) {
+    var name = array[i].Name;
+    var email = array[i].Email;
+    var cell = array[i].Cell;
+    var dynamicTag = '<tr>';
+        dynamicTag += '<td>' + name + '</td>';
+        dynamicTag += '<td>' + email + '</td>';
+        dynamicTag += '<td>' + cell + '</td>';
+        dynamicTag += '</tr>';
+        elementID('tableBody').innerHTML += dynamicTag;
+  }
+}
+
+function addFriendsBtnFunction() {
+  pushNameArray();
+  displayOnDocument();
+}
