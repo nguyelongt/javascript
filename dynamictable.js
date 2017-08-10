7fd37315f972be36ce1dd848a9bccf7c145963f6
NguyeLongT <nguyelongt@gmail.com>
2017-08-10 11:26:34 -0400

    Update

--- dynamictable.js
+++ dynamictable.js
@@ -2,20 +2,20 @@ function elementID(id) {
   return document.getElementById(id);
 }
 
-var friends = {}
-var array = []
+var friends = {};
+var array = [];
 
 function pushNameArray() {
 
   array = [];
 
-  var nameStored = elementID("nameInput");
-  var emailStored = elementID("emailInput");
-  var cellStored = elementID("cellInput");
-      friends.Name = nameStored.value;
-      friends.Email = emailStored.value;
-      friends.Cell = cellStored.value;
-      array.push(friends);
+  var nameStored = elementID('nameInput');
+  var emailStored = elementID('emailInput');
+  var cellStored = elementID('cellInput');
+  friends.Name = nameStored.value;
+  friends.Email = emailStored.value;
+  friends.Cell = cellStored.value;
+  array.push(friends);
 }
 
 function displayOnDocument() {
@@ -24,11 +24,11 @@ function displayOnDocument() {
     var email = array[i].Email;
     var cell = array[i].Cell;
     var dynamicTag = '<tr>';
-        dynamicTag += '<td>' + name + '</td>';
-        dynamicTag += '<td>' + email + '</td>';
-        dynamicTag += '<td>' + cell + '</td>';
-        dynamicTag += '</tr>';
-        elementID('tableBody').innerHTML += dynamicTag;
+    dynamicTag += '<td>' + name + '</td>';
+    dynamicTag += '<td>' + email + '</td>';
+    dynamicTag += '<td>' + cell + '</td>';
+    dynamicTag += '</tr>';
+    elementID('tableBody').innerHTML += dynamicTag;
   }
 }
 
